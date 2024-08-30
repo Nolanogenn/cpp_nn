00a97d3fc8ad05daa962d4624ab8c3a1cbd91dcc
gnolano <nolanogenn@gmail.com>
2024-08-30 15:07:00 +0000

    added an output layer

--- ffnn/ffnn.cpp
+++ ffnn/ffnn.cpp
@@ -12,6 +12,12 @@ double sigmoid(double i){
 	return 1/(1+e);
 }
 
+double tanh(double i){
+	double e_pos = std::exp(i);
+	double e_neg = std::exp(-i);
+	return (e_pos-e_neg)/(e_pos+e_neg);
+}
+
 double relu(double i) {
 	double tmp_return = 0;
 	if(i<=0){
@@ -96,31 +102,115 @@ struct Layer {
 		}
 	}
 };
+
+struct outputLayer {
+	Unit* arrayOfUnits;
+	int numNeurons;
+	int numW;
+	double *outputs;
+	double *softmaxOutputs;
+	double *inputs;
+
+	outputLayer(int lenN, int lenW) : numNeurons(lenN), numW(lenW){
+		outputs = new double[numNeurons];
+		softmaxOutputs = new double[numNeurons];
+		inputs = new double[lenW];
+		arrayOfUnits = static_cast<Unit*>(operator new[](numNeurons * sizeof(Unit)));
+		for(int i=0;i<numNeurons;i++)
+		{
+		new (&arrayOfUnits[i]) Unit(lenW);
+		for(int x =0; x<lenW; x++){
+			arrayOfUnits[i].W[x] = dist(re);
+			}
+		arrayOfUnits[i].b = 0;
+		}
+	}
+	~outputLayer(){
+		for (int i=0; i<numNeurons;i++){
+			arrayOfUnits[i].~Unit();
+		}
+		operator delete[](arrayOfUnits);
+		delete[] softmaxOutputs;
+		delete[] outputs;
+		delete[] inputs;
+	}
+	void printAll() const{
+		for (int i=0; i<numNeurons; i++){
+			arrayOfUnits[i].printData();
+		}
+	}
+	void makeInput(double vector[]) const{
+		for(int i=0; i<numW; i++){
+			inputs[i] = vector[i];
+		}
+	}
+	void forward() const{
+		for(int i=0; i<numNeurons;i++){
+			double temp_res;
+			for(int j=0; j<numW; j++)
+			{
+				temp_res += inputs[j]*arrayOfUnits[i].W[j];
+			}	
+			outputs[i] = temp_res;
+		}
+	}
+	void softmax() const{
+		double denom = 0;
+		for(int i=0; i<numNeurons;i++){
+			denom += std::exp(outputs[i]);
+		}
+		for(int i=0; i<numNeurons;i++){
+			softmaxOutputs[i] = std::exp(outputs[i])/denom;
+		}
+	}
+};
 int main()
 {
 
 	double* Layer::*output;
 	output = &Layer::outputs;
 
-	Layer* layer1 = new Layer(5,10,relu);
-	Layer* layer2 = new Layer(3,5,sigmoid);
+	double* outputLayer::*finalOutputpresoft;
+	finalOutputpresoft = &outputLayer::outputs;
+
+	double* outputLayer::*finalOutput;
+	finalOutput = &outputLayer::softmaxOutputs;
+
+	Layer* layer1 = new Layer(10,10,relu);
+	Layer* layer2 = new Layer(5,5,sigmoid);
+	outputLayer* finalLayer = new outputLayer(2,3);
 	
 	double x[10] = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0};
 	layer1->makeInput(x);
 	layer1->forward();
 
 	double* output1 = layer1->*output;
+	for(int i=0;i<10;i++){
+		std::cout << output1[i] << " ";
+	}
+	std::cout << std::endl;
 
 	layer2->makeInput(output1);
 	layer2->forward();
 
 	double* output2 = layer2->*output;
-	for(int i=0;i<3;i++){
+	for(int i=0;i<5;i++){
 		std::cout << output2[i] << " ";
+	}
+	std::cout << std::endl;
+
+	finalLayer->makeInput(output2);
+	finalLayer->forward();
+	finalLayer->softmax();
+
+	double* finalOutputs = finalLayer->*finalOutput;
+	for(int i=0;i<2;i++){
+		std::cout << finalOutputs[i] << " ";
 	}
 	std::cout << std::endl;
 
 	delete layer1;
 	delete layer2;
+	delete finalLayer;
 	return 0;
 }
